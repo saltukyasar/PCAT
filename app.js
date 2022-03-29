8637bd077ecf86097cf5548f7163d6d4da03cbfc
saltukyasar <saltukyasar@gmail.com>
2022-03-30 02:50:26 +0300

    static files upluoaded

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
-const fn = function () {
-  console.log('Function working');
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const myLogger = (req, res, next) => {
+  console.log('Middleware log1');
+  next();
 };
+const myLogger2 = (req, res, next) => {
+  console.log('Middleware log2');
+  next();
+};
+//Middlewares
+app.use(express.static('public'));
+app.use(myLogger);
+app.use(myLogger2);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`sunucu ${port} portunda calisiyor...`);
+});
