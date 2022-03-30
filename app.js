f262afabbc205dbf233b46720d8d639cf4b02d9e
saltukyasar <saltukyasar@gmail.com>
2022-03-31 01:11:03 +0300

    template engine

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
 
-const myLogger = (req, res, next) => {
-  console.log('Middleware log1');
-  next();
-};
-const myLogger2 = (req, res, next) => {
-  console.log('Middleware log2');
-  next();
-};
+//Template engine
+app.set('view engine', 'ejs');
+
 //Middlewares
 app.use(express.static('public'));
-app.use(myLogger);
-app.use(myLogger2);
 
+//Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add', (req, res) => {
+  res.render('add');
 });
 
 const port = 3000;
