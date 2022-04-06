f5ace571a02f93208dc2aa83f3f5e42b2128b064
saltukyasar <saltukyasar@gmail.com>
2022-04-06 11:02:59 +0300

    db CRUD

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
 
 const app = express();
+
+//connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //Template engine
 app.set('view engine', 'ejs');
 
 //Middlewares
 app.use(express.static('public'));
-app.use(express.urlencoded({extended:true}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Routes
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({}) 
+  res.render('index',{
+    photos
+  });
 });
+
 app.get('/about', (req, res) => {
   res.render('about');
 });
@@ -23,11 +35,10 @@ app.get('/add', (req, res) => {
   res.render('add');
 });
 
-app.post('/photos', (req, res) => {
-  console.log(req.body);
-  res.redirect('/')
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
 });
-
 
 const port = 3000;
 
