c11680000f246d889ef555e5997e1d493fce0a24
saltukyasar <saltukyasar@gmail.com>
2022-04-16 12:49:28 +0300

    can upload image files

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
 const ejs = require('ejs');
 const path = require('path');
+const fs = require('fs');
 const Photo = require('./models/Photo');
 
 const app = express();
@@ -19,22 +21,23 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(fileUpload());
 
 //Routes
 app.get('/', async (req, res) => {
-  const photos = await Photo.find({}) 
-  res.render('index',{
-    photos
+  const photos = await Photo.find({}).sort('-dateCreated');
+  res.render('index', {
+    photos,
   });
 });
 
 app.get('/photos/:id', async (req, res) => {
   //console.log(req.params.id)
   //res.render('photo');
-  const photo = await Photo.findById(req.params.id) 
+  const photo = await Photo.findById(req.params.id);
   res.render('photo', {
-    photo
-  })
+    photo,
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -45,8 +48,26 @@ app.get('/add', (req, res) => {
 });
 
 app.post('/photos', async (req, res) => {
-  await Photo.create(req.body);
-  res.redirect('/');
+  //console.log(req.files.image)
+  //await Photo.create(req.body);
+  //res.redirect('/');
+
+  const uploadDir = 'public/uploads';
+
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+
+  let uploadeImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
+
+  uploadeImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadeImage.name,
+    });
+    res.redirect('/');
+  });
 });
 
 const port = 3000;
